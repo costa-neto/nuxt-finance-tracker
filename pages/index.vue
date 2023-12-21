482209b3b7df3baec0a90cdf80a02b54dfcbfcf7
Neto Costa <neto1809costa@gmail.com>
2023-12-21 15:45:24 -0300

    feat: trends info and modal

--- pages/index.vue
+++ pages/index.vue
@@ -8,10 +8,37 @@
     </div>
   </section>
   <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10">
-    <Trend color="green" title="Income" :amount="4000" :last-amount="3000" :loading="isLoading" />
-    <Trend color="red" title="Expense" :amount="4000" :last-amount="5000" :loading="isLoading" />
+    <Trend color="green" title="Income" :amount="incomeTotal" :last-amount="3000" :loading="isLoading" />
+    <Trend color="red" title="Expense" :amount="expenseTotal" :last-amount="5000" :loading="isLoading" />
     <Trend color="green" title="Investments" :amount="4000" :last-amount="3000" :loading="isLoading" />
     <Trend color="red" title="Savings" :amount="4000" :last-amount="4100" :loading="isLoading" />
+  </section>
+
+  <section class="flex justify-between mb-10">
+    <div>
+      <h2 class="text-2xl font-extrabold">Transactions</h2>
+      <div class="text-gray-500 dark:text-gray-400">
+        You have {{ incomeCount }} incomes and {{ expenseCount }} expense in this period.
+      </div>
+    </div>
+    <div>
+      <UModal v-model="isOpen">
+        <UCard>
+          <template #header>
+            Add Transaction
+          </template>
+
+          <div>Hello!</div>
+        </UCard>
+      </UModal>
+      <UButton 
+        icon="i-heroicons-plus-circle" 
+        color="white" 
+        variant="solid"
+        label="Add"
+        @click="isOpen = true"
+      />
+    </div>
   </section>
 
   <section v-if="!isLoading">
@@ -36,6 +63,25 @@ const supabase = useSupabaseClient()
 const selectedView = ref(transactionViewOptions[1])
 const transactions = ref([])
 const isLoading = ref(false)
+const isOpen = ref(false)
+
+const income = computed(
+  () => transactions.value.filter(t => t.type === 'Income')
+)
+
+const expense = computed(
+  () => transactions.value.filter(t => t.type === 'Expense')
+)
+const incomeCount = computed(() => income.value.length)
+const expenseCount = computed(() => expense.value.length)
+
+const incomeTotal = computed(
+  () => income.value.reduce(((acc, cur) => acc + cur.amount), 0)
+)
+
+const expenseTotal = computed(
+  () => expense.value.reduce(((acc, cur) => acc + cur.amount), 0)
+)
 
 const fetchTransactions = async () => {
   isLoading.value = true
