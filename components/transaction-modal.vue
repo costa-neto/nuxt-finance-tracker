1dbd744a486b1f9060c12a56e072749011e31f01
Neto Costa <neto1809costa@gmail.com>
2023-12-23 20:50:52 -0300

    update nuxt/ui module and clear form

--- components/transaction-modal.vue
+++ components/transaction-modal.vue
@@ -7,39 +7,37 @@
 
       <UForm :state="state" :schema="schema" ref="form" @submit.prevent="save">
         <UFormGroup :required="true" label="Transaction Type" name="type" class="mb-4">
-          <USelect placeholder="Select the transaction type" :options="types" v-model="state.type"/>
+          <USelect placeholder="Select the transaction type" :options="types" v-model="state.type" />
         </UFormGroup>
 
         <UFormGroup label="Amount" :required="true" name="amount" class="mb-4">
-          <UInput type="number" placeholder="Amount" v-model.number="state.amount"/>
+          <UInput type="number" placeholder="Amount" v-model.number="state.amount" />
         </UFormGroup>
 
         <UFormGroup label="Transaction date" :required="true" name="created_at" class="mb-4">
-          <UInput type="date" icon="i-heroicons-calendar-days-20-solid" v-model="state.created_at"/>
+          <UInput type="date" icon="i-heroicons-calendar-days-20-solid" v-model="state.created_at" />
         </UFormGroup>
 
         <UFormGroup label="Description" hint="Optional" name="description" class="mb-4">
-          <UInput placeholder="Description" v-model="state.description"/>
+          <UInput placeholder="Description" v-model="state.description" />
         </UFormGroup>
 
         <UFormGroup :required="true" label="Category" name="category" class="mb-4" v-if="state.type === 'Expense'">
-          <USelect placeholder="Category" :options="categories" v-model="state.category"/>
+          <USelect placeholder="Category" :options="categories" v-model="state.category" />
         </UFormGroup>
 
         <UButton type="submit" color="black" variant="solid" label="Save" />
-        
       </UForm>
-
     </UCard>
   </UModal>
 </template>
 
 <script setup>
-import { categories, types } from '~/constants';
+import { categories, types } from '~/constants'
 import { z } from 'zod'
 
 const props = defineProps({
-  modelValue: Boolean,
+  modelValue: Boolean
 })
 const emit = defineEmits(['update:modelValue'])
 
@@ -48,6 +46,7 @@ const defaultSchema = z.object({
   description: z.string().optional(),
   amount: z.number().positive('Amount needs to be more than 0')
 })
+
 const incomeSchema = z.object({
   type: z.literal('Income')
 })
@@ -61,30 +60,40 @@ const investmentSchema = z.object({
 const savingSchema = z.object({
   type: z.literal('Saving')
 })
+
 const schema = z.intersection(
   z.discriminatedUnion('type', [incomeSchema, expenseSchema, investmentSchema, savingSchema]),
   defaultSchema
 )
+
 const form = ref()
+
 const save = async () => {
-  form.value.validate()
+  if (form.value.errors?.length) return
+
+  // Store into the supabase
 }
 
-
-const state = ref({
+const initialState = {
   type: undefined,
   amount: 0,
   created_at: undefined,
   description: undefined,
   category: undefined
+}
+const state = ref({
+  ...initialState
 })
+const resetForm = () => {
+  Object.assign(state.value, initialState)
+  form.value.clear()
+}
 
 const isOpen = computed({
   get: () => props.modelValue,
-  set: (value) => emit('update:modelValue')
+  set: (value) => {
+    if (!value) resetForm()
+    emit('update:modelValue', value)
+  }
 })
 </script>
-
-<style>
-
-</style>
